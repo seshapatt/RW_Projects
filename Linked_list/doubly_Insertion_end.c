e703a4a2afe0ba1741f6692ac6061bc235f477ad
seshapatt <garavindseshadri@gmail.com>
2020-04-13 15:36:55 -0700

    Insertion at end using doubly linked list

--- Linked_list/doubly_Insertion_end.c
+++ Linked_list/doubly_Insertion_end.c
@@ -4,47 +4,49 @@
 struct node
 {
     int data;
-    struct node *prev;
-    struct node*next;
+    struct node* prev;
+    struct node* next;
 };
 
-struct node*head;
+struct node* head;
 
 int main()
 {
     head=NULL;
-    printf("Doubly linked list Insertion at end\n");
-    Insert(5);
+    printf("Doubly linked list insertion at the end\n");
     Insert(9);
-    Insert(45);
     Insert(8);
+    Insert(4);
+    Insert(32);
     Print();
 }
 
 void Insert(int n)
 {
-    struct node *temp=(struct node*) malloc(sizeof(struct node));
-    struct node *temp2=(struct node*) malloc(sizeof(struct node));
+    struct node* temp=head;
+    struct node* temp2 = (struct node*) malloc(sizeof(struct node));
     if(head==NULL)
     {
-        (*temp).data=n;
-        (*temp).next=head;
-        (*temp).prev=head;
-        head=temp;
-    }
-    else
-    {
-        (*temp2).data=n;
-        (*temp2).next=head;
-        temp->prev=temp2;
+        temp2->prev=NULL;
+        temp2->data=n;
+        (*temp2).next=NULL;
         head=temp2;
+        return;
     }
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    (*temp2).data=n;
+    (*temp2).prev=head;
+    (*temp2).next=NULL;
+    (*temp).next=temp2;
 
 }
 
 void Print()
 {
-    struct node* temp=head;
+    struct node* temp= head;
     while(temp!=NULL)
     {
         printf("%d ",temp->data);
